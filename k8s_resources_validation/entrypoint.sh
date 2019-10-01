ff08bda172720bd7eea116bc62fd737bf510fb6c
ido <idobry@hotmail.co.il>
2019-10-01 22:08:28 +0300

    Create entrypoint.sh

---
+++ k8s_resources_validation/entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+msg2slack()
+{
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$1\"}" $SLACK_URL
+}
+
+mkdir -p $(helm home)/plugins
+helm plugin install https://github.com/instrumenta/helm-kubeval
+
+name=$(git log -1 --pretty=format:'%an')
+files=$(git diff HEAD^ --name-only)
+
+for file in $files; do
+
+    case "$file" in
+    charts\/*\/templates\/*) 
+        chart_name="$(cut -d'/' -f2 <<<"$file")"
+        chart=charts/$chart_name
+        if helm lint $chart && helm kubeval --ignore-missing-schemas $chart ; then
+            echo "${file} is valid"
+        else
+            msg2slack "Hey ${name}, ${file} that belongs to ${chart} is NOT valid"
+        fi
+        break;;
+    *.y*ml*) 
+        if kubeval --ignore-missing-schemas $file ; then
+            echo "${file} is valid"
+        else
+            msg2slack "Hey ${name}, ${file} is NOT valid"
+        fi
+        break;;
+    *) echo "${file} not a yaml file" ;;
+    esac
+
+done
