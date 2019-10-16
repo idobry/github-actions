85735282ecda774fdf28e2bd26119805a53fc784
ido <idobry@hotmail.co.il>
2019-10-16 09:01:23 +0300

    Update entrypoint.sh

--- k8s_resources_validation/entrypoint.sh
+++ k8s_resources_validation/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -x
+
 msg2slack()
 {
-    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$1\"}" $SLACK_URL
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$1\"}" "$SLACK_URL"
 }
 
-mkdir -p $(helm home)/plugins
+mkdir -p "$(helm home)/plugins"
 helm plugin install https://github.com/instrumenta/helm-kubeval
 
 name=$(git log -1 --pretty=format:'%an')
@@ -14,22 +16,22 @@ files=$(git diff HEAD^ --name-only)
 for file in $files; do
 
     case "$file" in
-    charts\/*) 
+    "charts\/*") 
         chart_name="$(cut -d'/' -f2 <<<"$file")"
         chart=charts/$chart_name
-        if helm lint $chart && helm kubeval --ignore-missing-schemas $chart ; then
+        if helm lint "$chart" && helm kubeval --ignore-missing-schemas "$chart" ; then
             echo "${file} is valid"
         else
             msg2slack "Hey ${name}, ${file} is NOT valid"
         fi
-        break;;
-    *.y*ml*) 
-        if kubeval --ignore-missing-schemas $file ; then
+        ;;
+    "*.y*ml*") 
+        if [ -f "$file" ] || kubeval --ignore-missing-schemas "$file" ; then
             echo "${file} is valid"
         else
             msg2slack "Hey ${name}, ${file} is NOT valid"
         fi
-        break;;
+        ;;
     *) echo "${file} not a yaml file" ;;
     esac
 
